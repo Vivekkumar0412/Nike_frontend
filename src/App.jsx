c7d7559d15102605f55013b73a3da802daf2db99
vivekkumar0412 <www.vivekkumar0412@gmail.com>
2025-03-11 21:57:14 +0530

    nike front end initial setup

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Nav from "./Components/Nav";
+import Hero from "./Components/Hero";
+import PopularProducts from "./Components/PopularProducts";
+import SuperQuality from "./Components/SuperQuality";
+import SpecialOffers from "./Components/SpecialOffers";
+import Services from "./Components/Services";
+import CustomerReview from "./Components/CustomerReview";
+import Suscribe from "./Components/Suscribe";
+import Footer from "./Components/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="padding bg-pale-blue ">
+        <CustomerReview />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Suscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/Suscribe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Suscribe() {
+  return (
+    <div>Suscribe</div>
+  )
+}
+
+export default Suscribe
